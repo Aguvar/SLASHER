88b479964fda72b84ffe460e45029fa4c76ab84b
Sebastian <ssosarippe@gmail.com>
2018-11-01 20:13:53 -0300

    User server webapi working, remoting configuration set up on host

---
+++ UserServer/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserServer.Exceptions;
+using UserServer.Services;
+
+namespace UserServer.Controllers
+{
+    [RoutePrefix("api")]
+    public class UserController : ApiController
+    {
+        private UserServices userServices;
+
+        [HttpGet]
+        [Route("users/")]
+        public IEnumerable<User> Get()
+        {
+            userServices = new UserServices();
+            return userServices.GetAll();
+        }
+
+        [HttpGet]
+        [Route("users/{nickname}")]
+        public User Get([FromUri]string nickname)
+        {
+            userServices = new UserServices();
+            return userServices.Get(nickname);
+        }
+
+        [HttpPost]
+        [Route("users/")]
+        public HttpResponseMessage Post([FromBody]User user)
+        {
+            userServices = new UserServices();
+            userServices.Add(user);
+            return Request.CreateResponse(HttpStatusCode.Created, user.Nickname);
+        }
+
+        [HttpPut]
+        [Route("users/{nickname}")]
+        public HttpResponseMessage Put([FromUri]string nickname, [FromBody]User user)
+        {
+            userServices = new UserServices();
+            try
+            {
+                userServices.Update(user, nickname);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            } catch(ElementNotFoundException e)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpDelete]
+        [Route("users/{nickname}")]
+        public HttpResponseMessage Delete([FromUri]string nickname)
+        {
+            userServices = new UserServices();
+            try
+            {
+                userServices.Delete(nickname);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (ElementNotFoundException e)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
--- UserServer/Global.asax.cs
+++ UserServer/Global.asax.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using UserServer.Services;
 
 namespace UserServer
 {
@@ -13,11 +14,30 @@ namespace UserServer
     {
         protected void Application_Start()
         {
+            SetupRemotingService();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void SetupRemotingService()
+        {
+            var remotingTcpChannel = new TcpChannel(7000);
+
+            ChannelServices.RegisterChannel(
+                remotingTcpChannel,
+                false);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(UserServices),
+                "RemoteUserServices",
+                WellKnownObjectMode.SingleCall);
+
+            Console.WriteLine("Server has started at: tcp://127.0.0.1/RemoteUserServices");
+            Console.ReadLine();
+            ChannelServices.UnregisterChannel(remotingTcpChannel);
         }
     }
 }
